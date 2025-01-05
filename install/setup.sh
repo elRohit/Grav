096019198d83a873350b03ca91aaeed61e409d0a
elRohit <r.kumar@sapalomera.cat>
2025-01-05 21:51:57 +0100

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,7 +1,11 @@
-#Actualitzar el sistema
-sudo apt-get update && sudo apt-get upgrade -y
-# Instal·lar dependències necessàries (webserver, php, unzip)
-sudo apt-get install -y apache2 php8.3 php8.3-curl php8.3-dom php8.3-gd php8.3-mbstring php8.3-xml php8.3-zip unzip
+# Actualitzar el sistema
+sudo apt-get update > /dev/null && sudo apt-get upgrade -y > /dev/null
+echo "Sistema actualitzat correctament."
+
+# Instal·lar dependències necessàries (servidor web, php, unzip)
+sudo apt-get install -y apache2 php8.1 php8.1-curl php8.1-dom php8.1-gd php8.1-mbstring php8.1-xml php8.1-zip unzip > /dev/null
+echo "Dependències(apache,php,unzip) instal·lades correctament."
+
 # Descarregar i descomprimir el GravCMS
 # Preguntar a l'usuari si vol la versió Grav Admin
 read -p "Vols la versió Grav Admin? (yes/no): " grav_admin
@@ -13,21 +17,29 @@ else
     download_url="https://getgrav.org/download/core/grav/1.7.48"
 fi
 
-# Descarregar i descomprimir el GravCMS la ruta on correspon
-wget $download_url 
-unzip 1.7.48 -d /var/www/html/
-#Configurar virtualhost nou per al GravCMS amb https i certificat autogenerat
-sudo a2enmod ssl
-sudo a2ensite default-ssl
-sudo systemctl restart apache2  
+# Descarregar i descomprimir el GravCMS a la ruta corresponent
+wget $download_url -O grav.zip > /dev/null
+echo "GravCMS descarregat correctament."
+unzip grav.zip -d /var/www/html/ > /dev/null
+echo "GravCMS descomprimit correctament."
+
+# Configurar un nou virtualhost per al GravCMS amb https i certificat autogenerat
+sudo a2enmod ssl > /dev/null
+echo "Mòdul SSL habilitat correctament."
+sudo a2ensite default-ssl > /dev/null
+echo "Lloc SSL habilitat correctament."
+sudo systemctl restart apache2 > /dev/null
+echo "Apache reiniciat correctament."
+
 # Crear un certificat letsencrypt
-sudo apt-get install -y certbot python3-certbot-apache
-sudo certbot --apache
+sudo apt-get install -y certbot python3-certbot-apache > /dev/null
+echo "Certbot instal·lat correctament."
+sudo certbot --apache > /dev/null
+echo "Certificat Let's Encrypt creat correctament."
+
 # Reiniciar el servei d'apache
-sudo systemctl restart apache2
+sudo systemctl restart apache2 > /dev/null
+echo "Apache reiniciat correctament."
+
 # Informar a l'usuari de la finalització de la instal·lació
 echo "Instal·lació finalitzada. Ja pots accedir al teu nou lloc web amb GravCMS a https://localhost"
-
-
-
-
